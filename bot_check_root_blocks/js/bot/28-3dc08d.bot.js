86942281e64db8430dc19e14ef43422d74eee69d
McSam <samyong@Sams-Mac.local>
2019-11-12 11:17:12 +0800

    Updates

---
+++ bot_check_root_blocks/js/bot/28-3dc08d.bot.js
@@ -0,0 +1,14 @@
+(window["webpackJsonpderiv_bot"] = window["webpackJsonpderiv_bot"] || []).push([[28],{
+
+/***/ "./src/scratch/help-content/help-string/sell_price.json":
+/*!**************************************************************!*\
+  !*** ./src/scratch/help-content/help-string/sell_price.json ***!
+  \**************************************************************/
+/*! exports provided: text, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"text\\\":[\\\"This block gives you the potential profit or loss if you decide to sell your contract. It can only be used within the “Sell conditions” root block.\\\",\\\"In the example below, the contract will only be sold if the potential profit or loss is more than the stake.\\\"]}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2NyYXRjaC9oZWxwLWNvbnRlbnQvaGVscC1zdHJpbmcvc2VsbF9wcmljZS5qc29uLmpzIiwic291cmNlcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/scratch/help-content/help-string/sell_price.json\n");
+
+/***/ })
+
+}]);
