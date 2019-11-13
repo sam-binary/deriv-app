b161f905cf3b3a019097813cf9e0afa45f28e1a0
McSam <samyong@Sams-Mac.local>
2019-11-13 15:29:29 +0800

    Updates

--- bot_search_improvement/service-worker.js
+++ bot_search_improvement/service-worker.js
@@ -15,7 +15,7 @@ importScripts("/bot_search_improvement/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/bot_search_improvement/workbox-v4.3.1"});
 
 importScripts(
-  "/bot_search_improvement/precache-manifest.fd23e748a1ccfa69c3d6959e168e4148.js"
+  "/bot_search_improvement/precache-manifest.2f5727c131eb4db766f60ce0a3700435.js"
 );
 
 workbox.core.skipWaiting();
