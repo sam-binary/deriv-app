9b14ba640d3201513ee1ea539136a6e14f620d6b
McSam <samyong@Sams-Mac.local>
2019-11-15 18:05:37 +0800

    Updates

---
+++ br_error_dynamic_block/js/bot/bot.last_digit-json.1431a07c4168885a5e9f.js
@@ -0,0 +1,14 @@
+(window["webpackJsonpderiv_bot"] = window["webpackJsonpderiv_bot"] || []).push([["last_digit-json"],{
+
+/***/ "./src/scratch/help-content/help-string/last_digit.json":
+/*!**************************************************************!*\
+  !*** ./src/scratch/help-content/help-string/last_digit.json ***!
+  \**************************************************************/
+/*! exports provided: text, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"text\\\":[\\\"This block gives you the last digit of the latest tick value of the selected market. If the latest tick value is 1410.90, this block will return 0. It’s useful for digit-based contracts such as Even/Odd, Matches/Differs, or Higher/Lower.\\\"]}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2NyYXRjaC9oZWxwLWNvbnRlbnQvaGVscC1zdHJpbmcvbGFzdF9kaWdpdC5qc29uLmpzIiwic291cmNlcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/scratch/help-content/help-string/last_digit.json\n");
+
+/***/ })
+
+}]);
