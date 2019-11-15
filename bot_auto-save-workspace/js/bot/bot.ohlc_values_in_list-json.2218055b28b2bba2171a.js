223d1aadfe136bcfdd8777af84c47a81af087966
McSam <samyong@Sams-Mac.local>
2019-11-15 17:29:08 +0800

    Updates

---
+++ bot_auto-save-workspace/js/bot/bot.ohlc_values_in_list-json.2218055b28b2bba2171a.js
@@ -0,0 +1,14 @@
+(window["webpackJsonpderiv_bot"] = window["webpackJsonpderiv_bot"] || []).push([["ohlc_values_in_list-json"],{
+
+/***/ "./src/scratch/help-content/help-string/ohlc_values_in_list.json":
+/*!***********************************************************************!*\
+  !*** ./src/scratch/help-content/help-string/ohlc_values_in_list.json ***!
+  \***********************************************************************/
+/*! exports provided: text, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"text\\\":[\\\"This block gives you the selected candle value from a list of candles. You can choose from open price, close price, high price, low price, and open time.\\\",\\\"This block requires a list of candles as an input parameter.\\\",\\\"In this example, the open prices from a list of candles are assigned to a variable called “cl”.\\\"]}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2NyYXRjaC9oZWxwLWNvbnRlbnQvaGVscC1zdHJpbmcvb2hsY192YWx1ZXNfaW5fbGlzdC5qc29uLmpzIiwic291cmNlcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/scratch/help-content/help-string/ohlc_values_in_list.json\n");
+
+/***/ })
+
+}]);
