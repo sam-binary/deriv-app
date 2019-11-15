1fdc627ce1ce780982e74946486d1f118d8ff51e
McSam <samyong@Sams-Mac.local>
2019-11-15 17:49:38 +0800

    Updates

---
+++ br_quick_strategy_improvement/js/bot/bot.check_direction-json.4bd6bf89237e8dfe9a2c.js
@@ -0,0 +1,14 @@
+(window["webpackJsonpderiv_bot"] = window["webpackJsonpderiv_bot"] || []).push([["check_direction-json"],{
+
+/***/ "./src/scratch/help-content/help-string/check_direction.json":
+/*!*******************************************************************!*\
+  !*** ./src/scratch/help-content/help-string/check_direction.json ***!
+  \*******************************************************************/
+/*! exports provided: text, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"text\\\":[\\\"This block is used to determine if the market price moves in the selected direction or not. It gives you a value of “True” or “False”.\\\",\\\"Examples:\\\",\\\"1. If the selected direction is “Rise”, and the previous tick value is less than the current tick value, the output will be “True”. Otherwise, the output will be an empty string.\\\",\\\"2. If the selected direction is “Fall”, and the previous tick value is more than the current tick value, the output will be “True”. Otherwise, the output will be an empty string.”\\\"]}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2NyYXRjaC9oZWxwLWNvbnRlbnQvaGVscC1zdHJpbmcvY2hlY2tfZGlyZWN0aW9uLmpzb24uanMiLCJzb3VyY2VzIjpbXSwibWFwcGluZ3MiOiIiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/scratch/help-content/help-string/check_direction.json\n");
+
+/***/ })
+
+}]);
