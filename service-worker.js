caddae3a53d386f657480653037a3a45d30d38b2
McSam <samyong@Sams-Mac.local>
2019-11-15 17:32:48 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4383e7cb3f646d633645d19df1874558.js"
+  "/precache-manifest.cd219af62c97ecba5c044e81b35faa85.js"
 );
 
 workbox.core.skipWaiting();
