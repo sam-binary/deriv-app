dd4f8eba0e6b457488c0c11ce2c4f0a93f0a6225
System Administrator <root@Sams-Mac.local>
2019-11-27 14:23:25 +0800

    Updates

---
+++ br_add_forum/js/bot/bot.ohlc_values-json.3a8bdbcf0c9617812af9.js
@@ -0,0 +1,14 @@
+(window["webpackJsonpderiv_bot"] = window["webpackJsonpderiv_bot"] || []).push([["ohlc_values-json"],{
+
+/***/ "./src/scratch/help-content/help-string/ohlc_values.json":
+/*!***************************************************************!*\
+  !*** ./src/scratch/help-content/help-string/ohlc_values.json ***!
+  \***************************************************************/
+/*! exports provided: text, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"text\\\":[\\\"This block gives you the selected candle value from a list of candles within the selected time interval. You can choose from open price, close price, high price, low price, and open time.\\\",\\\"In this example, the open prices from a list of candles are assigned to a variable called “candle_list”.\\\"]}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2NyYXRjaC9oZWxwLWNvbnRlbnQvaGVscC1zdHJpbmcvb2hsY192YWx1ZXMuanNvbi5qcyIsInNvdXJjZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/scratch/help-content/help-string/ohlc_values.json\n");
+
+/***/ })
+
+}]);
