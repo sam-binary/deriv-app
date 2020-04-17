8bdb6e9249f7608ce359f45de5b4e70096a2e46c
McSam <samyong@Sams-Mac.local>
2020-04-17 09:52:14 +0800

    Updates

--- br_animation/service-worker.js
+++ br_animation/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_animation/precache-manifest.f24a0b04531c0ecc3acb4fc91ddbcbe5.js"
+  "/br_animation/precache-manifest.f47e4c0ae6129aec18d32c43046d7cb5.js"
 );
 
 workbox.core.skipWaiting();
