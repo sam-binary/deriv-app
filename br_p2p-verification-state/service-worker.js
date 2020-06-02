c4853d62387533ed573614a6802e4bde2c115b78
McSam <samyong@Sams-Mac.local>
2020-06-02 15:33:15 +0800

    Updates

--- br_p2p-verification-state/service-worker.js
+++ br_p2p-verification-state/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_p2p-verification-state/precache-manifest.397311cb4635fb080d9244ca4c5e5980.js"
+  "/br_p2p-verification-state/precache-manifest.1c01ed17729d11b11fb98c84d1f0eb9a.js"
 );
 
 workbox.core.skipWaiting();
